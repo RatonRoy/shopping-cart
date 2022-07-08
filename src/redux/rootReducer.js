47e0758c9d160e69cb55e238ea510b8a2aef2639
RatonRoy <royraton18@gmail.com>
2022-07-09 05:48:05 +0600

    complete the shopping types and actions

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,4 +1,9 @@
 import { combineReducers } from "redux";
 
+import shoppingReducer from "./Shopping/shopping-reducer";
+
+const rootReducer = combineReducers({
+  shop: shoppingReducer,
+});
 
 export default rootReducer;
